1cea369349061c1d906e18f40c0819bd67242e9f
Andrew Braun <37201987+andrew-braun@users.noreply.github.com>
2020-06-06 13:02:21 +0400

    deleted initial experiment, added Card element

---
+++ src/Card.js
@@ -0,0 +1,14 @@
+import React from 'react':
+
+const Card = () => {
+    return (
+        <div>
+            <img src=""/>
+        </div>
+        <div>
+            <h2>Jane Doe</h2>
+            <p>jane.doe@gmail.com</p>
+        </div>
+    )
+}
+
