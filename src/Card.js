785143b4681046546f3dbc6078fdb96b48ac12da
Andrew Braun <37201987+andrew-braun@users.noreply.github.com>
2020-06-06 18:23:35 +0400

    updates

--- src/Card.js
+++ src/Card.js
@@ -5,7 +5,7 @@ const Card = ({ id, name, email }) => {
         <div className="robot-card bg-light-green br3 pa3 ma2 bw2 shadow-5 grow">
             <h1 className="tc">RoboFriend</h1>
             <div className="tc">
-                <img src={`https://robohash.org/${id}?size=200x200`} alt="profile"/>
+                <img src={`https://robohash.org/${name}?size=200x200`} alt="profile"/>
                 <h2>{name}</h2>
                 <p>{email}</p>
             </div>
