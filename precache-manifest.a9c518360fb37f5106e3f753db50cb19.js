7377856e74fecf5994736eabbbafa5570e448ba0
Andrew Braun <37201987+andrew-braun@users.noreply.github.com>
2020-06-08 21:54:11 +0400

    Updates

---
+++ precache-manifest.a9c518360fb37f5106e3f753db50cb19.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dfd783ab1666e92d95df9f6417bd876c",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "0569009c3ba5040cd268",
+    "url": "/robofriends/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "5bd92cf62792240e0036",
+    "url": "/robofriends/static/css/main.efde9753.chunk.css"
+  },
+  {
+    "revision": "0569009c3ba5040cd268",
+    "url": "/robofriends/static/js/2.935cd7c0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robofriends/static/js/2.935cd7c0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5bd92cf62792240e0036",
+    "url": "/robofriends/static/js/main.6341356d.chunk.js"
+  },
+  {
+    "revision": "99557a09ab68caec6407",
+    "url": "/robofriends/static/js/runtime-main.229ccf3b.js"
+  },
+  {
+    "revision": "73c304b610301b6a74733fcdb2a16334",
+    "url": "/robofriends/static/media/MADEEvolveSansEVO-Bold.73c304b6.woff"
+  },
+  {
+    "revision": "d70b27901f57de3e71f1289715050a89",
+    "url": "/robofriends/static/media/MADEEvolveSansEVO-Bold.d70b2790.woff2"
+  }
+]);
